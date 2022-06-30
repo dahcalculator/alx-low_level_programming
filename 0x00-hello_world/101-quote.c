75c05a08ce691c27b88a322448f1abc848071d9b
Jude Saleh <judesaleh30@gmail.com>
2022-06-30 16:27:21 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,15 +9,4 @@ int main(void)
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, ch, sizeof(ch));
 	return (1);
-}#include <stdio.h>
-#include <unistd.h>
-/**
- *  * main - Entry point
- *   * Return:1
- *    */
-int main(void)
-{
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
-	return (1);
 }
