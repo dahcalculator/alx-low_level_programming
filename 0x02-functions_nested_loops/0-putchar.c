4865c16675380df6752adeab38118d51d9723fd1
Jude Saleh <judesaleh30@gmail.com>
2022-07-06 19:21:21 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,29 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *  main - main block
- * 
- * Return: 0
- **/
+ *  * print_string - print _putchar followed by newline
+ *   *
+ *    * Return: void
+ *     */
+void print_string(void)
+{
+		char s[10] = "_putchar\n";
+			int i = 0;
+
+				while (s[i])
+						{
+									_putchar(s[i]);
+											i++;
+												}
+}
+
+/**
+ *  * main - entry point
+ *   *
+ *    * Return: always 0
+ *     */
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	return (0);
+		print_string();
+			return (0);
 }
