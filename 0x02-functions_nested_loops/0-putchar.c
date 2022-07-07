40fee3b2083746e11bfa7998293fa2991c32b6ee
Jude Saleh <judesaleh30@gmail.com>
2022-07-06 18:50:02 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+/**
+ *  main - main block
+ * 
+ * Return: 0
+ **/
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
